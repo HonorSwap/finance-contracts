266d87880095bc2ef88c5cf338ee2fa50ec005f7
Mehmet ERgenekon <PROMERGEN@GMAil.com>
2023-01-03 12:44:55 +0300

    lot

--- contracts/HonorLotteryHUSD.sol
+++ contracts/HonorLotteryHUSD.sol
@@ -13,7 +13,6 @@ contract HonorLotteryHUSD is Ownable {
         uint256 startBlock;
         uint256 endBlock;
         uint256 balanceStart;
-        uint256 ticketPrice;
         uint256 totalPrize;
         uint8[6][] tickets;
         uint8[6] winNumbers;
@@ -21,8 +20,9 @@ contract HonorLotteryHUSD is Ownable {
         uint32 win4Count;
         uint32 win5Count;
         uint32 win6Count;
-        bool checked;
+        uint8 status;
         uint ticketCount;
+        address[] users;
     }
 
     mapping(uint=>Lottery) public _lotteries;
@@ -30,21 +30,21 @@ contract HonorLotteryHUSD is Ownable {
     uint public currentLotteryID;
     
     mapping(address=>mapping(uint=>uint8[6][])) public _userTickets;
+    mapping(address=>mapping(uint=>uint)) public _userLotUserIDs;
     mapping(address=>uint[]) public userLotteries;
     address public _feeTo;
-    uint256 _FEE=5;
+    uint256 _FEE=30;
     
     address public _lotteryTicket;
     address public _hnrusd;
-
-
-    uint[] _checkLottery;
 
     uint256 _freeBalance;
 
     uint _MAXNUMBER=51;
 
-    uint256 _priceTicket=2 * 10**17;
+    uint256 _prizeTicket=2 * 10**17;
+    uint256 _maxUserTicket=100;
+
 
 
     function getTicketPrice(uint count) public view returns(uint256) {
@@ -59,26 +59,34 @@ contract HonorLotteryHUSD is Ownable {
 
         require(count<=20 && count>0,"MAX TICKET 20");
 
-        uint256 price=getTicketPrice(lottery.ticketPrice).mul(count);
+        uint userCount=_userTickets[msg.sender][lotteryID].length;
+        require((userCount+count)<=_maxUserTicket,"USER MAX TICKET");
+
+        
+        uint256 prize=getTicketPrice(lottery.ticketPrize);
         if(count>=5 && count<10)
         {
-            price=price.mul(9).div(10);
+            prize=prize.mul(9).div(10);
         }
         else if(count>=10 && count<15)
         {
-            price=price.mul(85).div(100);
+            prize=prize.mul(85).div(100);
         }
         else if(count>15)
         {
-            price=price.mul(4).div(5);
+            prize=prize.mul(4).div(5);
         }
-        uint256 fee=price.mul(_FEE).div(1000);
 
-        IERC20(_hnrusd).transferFrom(msg.sender, address(this), price);
-        IERC20(_hnrusd).transfer(_feeTo, fee);
-        price -= fee;
-        lottery.totalPrize=lottery.totalPrize.add(price);
+        IERC20(_hnrusd).transferFrom(msg.sender, address(this), prize);
         
+        
+        lottery.totalPrize=lottery.totalPrize + prize;
+        
+        if(userCount==0)
+        {
+            addUserLottery(msg.sender, lotteryID);
+        }
+
         for(uint i=0;i<count;i++)
         {
             lottery.tickets.push(tickets[i]);
@@ -86,21 +94,40 @@ contract HonorLotteryHUSD is Ownable {
         }
     }
 
+    function addUserLottery(address user,uint lotteryID) private returns(uint) {
+        Lottery storage lottery=_lotteries[lotteryID];
+        userLotteries[user].push(lotteryID);
+        uint userID=lottery.users.length;
+        lottery.users.push(user);
+        _userLotUserIDs[user][lotteryID]=userID;
+        return userID;
+    }
+    function checkUserCount(address user,uint lotID,uint count) private returns(uint) {
+        uint userCount=_userTickets[user][lotID].length;
+        require((userCount+count)<=_maxUserTicket,"USER MAX TICKET");
+        return userCount;
+    }
     function playFreeTickets(uint8[6][] memory tickets,uint lotteryID) public {
         Lottery storage lottery=_lotteries[lotteryID];
-        
+        require(lottery.startBlock>=block.number && lottery.endBlock<block.number,"ERROR LOT ID");
+
         uint256 count=tickets.length;
-        uint256 price=lottery.ticketPrice.mul(count).mul(1000000);
+        uint256 price=lottery.ticketPrize.mul(count).mul(1e18);
 
         ILotteryTicket(_lotteryTicket).burnTicket(price,msg.sender);
-
+        
+        uint userCount=checkUserCount(msg.sender, lotteryID, count);
+        
+        if(userCount==0)
+        {
+            addUserLottery(msg.sender, lotteryID);
+        }
         for(uint i=0;i<count;i++)
         {
             lottery.tickets.push(tickets[i]);
             _userTickets[msg.sender][lotteryID].push(tickets[i]);
         }
     }
-
 
 
     function getWinNumber(uint blockNumber,uint ticketCount,uint errorNum) public view returns(uint) {
@@ -108,52 +135,56 @@ contract HonorLotteryHUSD is Ownable {
         return num+1;
     }
 
-    function checkWinNumbers(uint blockNumber,uint ticketCount) public view returns(uint8[6] memory wins)
-    {
-
-        uint errorNum=0;
-
-        uint winNum=getWinNumber(blockNumber, ticketCount,errorNum);
-        wins[0]=uint8(winNum);
-        uint checked=1;
-        bool error=false;
-        for(uint i=1;i<6;i++)
-        {
-            if(error)
-            {
-                i-=1;
-                errorNum++;
-            }
-
-            uint winNum=uint8(getWinNumber(blockNumber, ticketCount,errorNum));
-            
-           
-            for(uint y=0;y<checked;y++)
-            {
-                if(wins[y]==winNum)
-                {
-                    error=true;
-                    break;
-                }
-            }
-            if(!error)
-            {
-                wins[i]=uint8(winNum);
-                
-                blockNumber++;
-                errorNum=0;
-                checked++;
-            }
-        }
+    function getLotteryWinNumber(uint lotID,uint next,uint errorNum) public view returns(uint) {
+        Lottery memory lottery=_lotteries[lotID];
+        uint256 bHash=blockhash(lottery.endBlock+next);
+        uint num=uint(keccak256(abi.encodePacked(bHash,lottery.ticketCount,errorNum))) % _MAXNUMBER;
+        return num+1;
     }
-    function finishLottery(uint lotID) public onlyOwner {
+
+    function finishLottery(uint lotID,uint8[6] calldata winNumbers) public onlyOwner {
         Lottery storage lottery=_lotteries[lotID];
         uint lastBlock=lottery.endBlock + 6;
 
         require(block.number>=lastBlock && lottery.endBlock>0,"ERROR LOTTERY TIME");
-        lottery.winNumbers=checkWinNumbers(lottery.endBlock,lottery.tickets.length);
+        lottery.winNumbers=winNumbers;
         lottery.ticketCount=lottery.tickets.length;
+        lottery.status=1; //Check Tickets
+    }
+
+    function checkUserTickets(uint lotID) public {
+        Lottery memory lottery=_lotteries[lotID];
+        require(lottery.status==2,"NOT FINISH CHECK");
+        uint8[6][] memory tickets=_userTickets[msg.sender][lotID];
         
+
+        uint count=tickets.length;
+        require(count>0,"NOT USER TICKET");
+
+        uint8[6] memory wins=lottery.winNumbers;
+        uint[7] memory userWins;
+        uint win=0;
+        for(uint i=0;i<count;i++)
+        {
+            win=checkTicket(lottery.winNumbers,tickets[i]);
+            userWins[win]++;
+        }
+
+    }
+
+    function checkTicket(uint8[6] memory winNumber,uint8[6] memory check) public returns(uint) {
+        uint win=0;
+        for(uint x=0;x<6;x++) {
+            for(uint y=0;y<6;y++)
+            {
+                if(check[x]==winNumber[y])
+                {
+                    win++;
+                    break;
+                }
+            }
+        }
+        return win;
     }
     function checkUserTickets(address user,uint lotID) public view returns(uint[7] memory wins) {
         Lottery memory lottery=_lotteries[lotID];
